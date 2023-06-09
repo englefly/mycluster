32bd36c7c8d91a5baf1b1a089fe790966facec38
minghong <minghong.zhou@163.com>
2023-06-09 21:37:58 +0800

    move doris-meta out of deloy dir

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 cp -r ../doris/output/fe/ fe
+cp fe_custom.conf fe/conf/
+
 cp -r ../doris/output/be/ be1
 cp -r ../doris/output/be/ be2
 cp -r ../doris/output/be/ be3
-cp fe_custom.conf fe/conf/
 cp be_custom.conf1 be1/conf/be_custom.conf
 cp be_custom.conf2 be2/conf/be_custom.conf
 cp be_custom.conf3 be3/conf/be_custom.conf
---
+++ deploy_fe.sh
@@ -0,0 +1,4 @@
+rm -r fe
+mkdir fe
+cp -r ../doris/output/fe/* fe/
+cp fe_custom.conf fe/conf/
